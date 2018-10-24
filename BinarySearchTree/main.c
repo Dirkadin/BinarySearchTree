f011f280ddac121e3d21d813182395ccfa46e2e6
dirk <dirkadin@gmail.com>
2018-10-24 11:05:44 -0500

    Initial Commit

---
+++ BinarySearchTree/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BinarySearchTree
+//
+//  Created by Dirk on 10/24/18.
+//  Copyright © 2018 Dirk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
